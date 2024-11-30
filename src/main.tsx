45737f1c444fea20b42d1bfeb02a952c0fb81b60
ihebchebbi1998tn <iheb.chebbi@lcieducation.net>
2024-11-30 19:40:35 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n'
+import { useTranslation } from 'react-i18next'
+
+const RootComponent = () => {
+  const { i18n } = useTranslation()
+
+  useEffect(() => {
+    const rootElement = document.getElementById('root')
+    if (rootElement) {
+      if (i18n.language === 'ar') {
+        rootElement.classList.add('rtl')
+      } else {
+        rootElement.classList.remove('rtl')
+      }
+    }
+  }, [i18n.language])
+
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootComponent />
   </StrictMode>,
 )
