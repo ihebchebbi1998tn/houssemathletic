d506c182aae6c0b64764291bca7d0f6f0d465a9f
ihebchebbi1998tn <iheb.chebbi@lcieducation.net>
2024-11-30 19:33:06 +0100

    commit22

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,37 +7,37 @@ const images = [
   {
     src: "https://i.ibb.co/2tjDN9t/468152940-18470446702050161-4099700776036128294-n.jpg",
     width: 800,
-    height: 600,
+    height: 900,
     title: "Image 1"
   },
   {
     src: "https://i.ibb.co/8BQmpVd/467318711-18284965693243085-4665614552950010621-n.jpg",
     width: 800,
-    height: 600,
+    height: 900,
     title: "Image 2"
   },
   {
     src: "https://i.ibb.co/1XFr8kt/467538818-18284965720243085-2225549853862629879-n.jpg",
     width: 800,
-    height: 600,
+    height: 900,
     title: "Image 3"
   },
   {
     src: "https://i.ibb.co/ws6XMxT/AR406752.jpg",
     width: 800,
-    height: 600,
+    height: 900,
     title: "AR406752"
   },
   {
     src: "https://i.ibb.co/C86L86R/467178043-18284965738243085-6012870816686190413-n.jpg",
     width: 800,
-    height: 600,
+    height: 900,
     title: "Image 4"
   },
   {
     src: "https://i.ibb.co/W3KgQm8/467117849-18284965756243085-7619141833982696844-n.jpg",
     width: 800,
-    height: 600,
+    height: 900,
     title: "Image 5"
   }
 ];
