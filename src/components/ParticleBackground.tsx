7c70162d47217327cbdd757aac6c5fc1b1bd0b3a
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2024-12-01 12:01:26 +0100

    Update ParticleBackground.tsx

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -12,7 +12,7 @@ const ParticleBackground: React.FC = () => {
         top: 0,
         left: 0,
         zIndex: -1,
-        backgroundImage: 'url("https://i.ibb.co/vmM6VF7/467509617-9386018204761354-3094294667648383955-n.jpg")',
+        backgroundImage: 'url("https://i.ibb.co/drf5D6p/c0c42ef2-66a8-4b91-9886-372f2a836848.jpg")',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
