e6291c0b74e795fce12e4f14fc5a187e1b50e9ae
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2024-12-03 11:00:01 +0100

    refix

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -1,40 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const ParticleBackground: React.FC = () => {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <div 
       style={{ 
         margin: 0,
         padding: 0,
-        width: isMobile ? '100vw' : '70vw', // Full width on mobile, 70% width on web
+        width: '100vw',
         height: '100vh',
         position: 'fixed',
         top: 0,
-        left: '50%',  // Center horizontally for non-mobile screens
-        transform: 'translateX(-50%)', // Ensure it's centered on larger screens
+        left: 0,
         zIndex: -1,
         backgroundImage: 'url("https://i.ibb.co/drf5D6p/c0c42ef2-66a8-4b91-9886-372f2a836848.jpg")',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         backgroundAttachment: 'fixed',
-        backgroundColor: 'black',  // Black background
+        backgroundColor: 'black', // Changed to black
       }}
     >
       <div 
@@ -44,7 +27,7 @@ const ParticleBackground: React.FC = () => {
           left: 0,
           width: '100%',
           height: '100%',
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',  // Darker overlay
+          backgroundColor: 'rgba(0, 0, 0, 0.7)', // Darker overlay
         }}
       />
     </div>
